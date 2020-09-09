910bb6f56ced316153d4f7509c6763f90d20d66c
Yaroslav Skachkov <ikanom96@gmail.com>
2020-09-09 15:13:45 +0300

    Feed item model implemented.

---
+++ NewsAgregator/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  NewsAgregator
+//
+//  Created by Ярослав on 9/9/20.
+//  Copyright © 2020 YaroslavSkachkov. All rights reserved.
+//
+
+import UIKit
+
+struct FeedItem {
+    var title: String
+    var description: String
+    var img: UIImage
+    var url: String
+    var source: String
+}
